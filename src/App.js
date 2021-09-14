08628dd8b1a2025856f49572697928a9b20be404
rmahato64 <rmahato64@gmail.com>
2021-09-14 16:48:50 +0300

    App component  modified

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ function App() {
     <div>
       <h2>Let's get started</h2>
       <ExpensesItem title="Car Insurance" amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpensesItem title="Car Insurance" amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpensesItem title="Car Insurance" amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpensesItem title="Car Insurance" amount={expenses[3].amount} date={expenses[3].date}/>
+      <ExpensesItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+      <ExpensesItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+      <ExpensesItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
 
 
     </div>
