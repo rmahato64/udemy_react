2ff951e797ccbedb874aa23189f58eb33eb5361e
rmahato64 <rmahato64@gmail.com>
2021-09-16 00:48:00 +0300

    form component added to add data to table changed with state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Expenses } from "./components/Expenses";
+import NewExpense from './components/NewExpense'
 
 const App=()=> {
 
@@ -26,6 +27,7 @@ const App=()=> {
   return (
     <div>
       <h2>Let's get started</h2>
+      <NewExpense />
       <Expenses expense={expenses}/>
 
     </div>
